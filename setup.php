8fb38a137a720610b5f99bb85b12b583543bcb14
Bestijan <nemanja.lazarevic.laza@gmail.com>
2018-04-01 01:31:06 +0200

    Add files via upload

---
+++ setup.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$host = getenv('MYSQL_SERVICE_HOST');
+$db_user = getenv('username');
+$db_password = getenv('password');
+$db_name = getenv('name');
+$con = mysqli_connect($host, $db_user, $db_password, $db_name);
+
+if (!$con){
+	
+}
+
+?>
