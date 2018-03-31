0974e1017de86ece00a25faaba68b44ee67f1393
Bestijan <nemanja.lazarevic.laza@gmail.com>
2018-03-31 16:02:19 +0200

    Update init.php

--- init.php
+++ init.php
@@ -1,10 +1,10 @@
 <?php
 
 
-$host = 'fdb18.awardspace.net';
-$db_user = '2677711_traffichelper';
-$db_password = '';
-$db_name = '2677711_traffichelper';
+$host = getenv('MYSQL_SERVICE_HOST');
+$db_user = getenv('username');
+$db_password = getenv('password');
+$db_name = getenv('name');
 
 $con = mysqli_connect($host, $db_user, $db_password, $db_name);
 
