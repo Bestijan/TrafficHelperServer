702880d2588648e10e2656377a5174b47931c5d3
Bestijan <nemanja.lazarevic.laza@gmail.com>
2018-04-02 00:15:34 +0200

    Update get_my_places.php

--- get_my_places/get_my_places.php
+++ get_my_places/get_my_places.php
@@ -4,44 +4,34 @@ require_once dirname(__FILE__).'/../init.php';
 
 $json = array();
 $json["id"] = "get_my_places";
+$json["result"] = "ok";
 
 if (isset($_POST) && isset($_POST['username'])){
 
 	$username = $_POST['username'];
 	$waypoint = 'Waypoint';
 	
-	$relative_path = 'http://traffic-helper-traffic-helper-server.7e14.starter-us-west-2.openshiftapps.com/img/';
-	
 	$sql_my_places = "select * from place_event where Username = '$username' AND Name <> '$waypoint'";
 	
 	$result_my_places = mysqli_query($con, $sql_my_places);
 
 	if ($result_my_places){
-		$json["result"] = "ok";
-		
 		$i = 0;
-		$files = scandir($relative_path.'img_my_places/');
-		unset($files[0]);
-		unset($files[1]);
 		
 		while ($res = mysqli_fetch_assoc($result_my_places)){
 			$json["my_places"][$i] = $res;
-			$path = $relative_path.'img_my_places/'.$username.'_'.$res["ID"].'.jpg';
-			$jpg = file_get_contents("$path");
-			$json["my_places"][$i]["my_place_img"] = base64_encode($jpg);
-			$i = $i + 1;
+			//$path = $relative_path.'img_my_places/'.$username.'_'.$res["ID"].'.jpg';
+			//$jpg = file_get_contents("$path");
 			
-			if (($key = array_search($username.'_'.$res["ID"].'.JPEG', $files)) !== false) {
-				unset($files[$key]);
-			}
+			$ID = $username.'_'.$res["ID"];
+			$sql = "select img from place_event_img where ID = '".$ID."'";	
+			$jpg = mysqli_query($con, $sql)->fetch_row()[0];	
+			
+			$json["my_places"][$i]["my_place_img"] = $jpg;
+			$i = $i + 1;
 		}
 		
-		foreach($files as $file){
-			$path = $relative_path.'img_my_places/'.$file;
-			unlink($path);
-		}
-		
-		$json['files'] = $files;
+	
 	}
 	else {
 		$json["result"] = mysqli_error($con);
