d8321a2c311eea8ced34562fd3f7371bb28d1a5b
Bestijan <nemanja.lazarevic.laza@gmail.com>
2018-04-01 17:25:53 +0200

    Update setup_.php

--- setup_.php
+++ setup_.php
@@ -12,7 +12,7 @@ if ($con){
 	    PE VARCHAR(5),
 	    MI VARCHAR(7),
 	    Name VARCHAR(100),
-	    Date_time timestamp CURRENT_TIMESTAMP,
+	    Date_time timestamp NOW(),
 	    Lat double,
 	    Lon double
             )";
