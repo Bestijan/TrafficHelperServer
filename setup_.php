66e66eace4401786735f3837e3e89a7a32d0e69b
Bestijan <nemanja.lazarevic.laza@gmail.com>
2018-04-01 03:20:39 +0200

    Update setup_.php

--- setup_.php
+++ setup_.php
@@ -3,18 +3,23 @@ $host = getenv('MYSQL_SERVICE_HOST');
 $db_user = getenv('username');
 $db_password = getenv('password');
 $db_name = getenv('name');
+
 $con = mysqli_connect($host, $db_user, $db_password, $db_name);
+if ($con){
+	$sql = "CREATE TABLE users (
+            username VARCHAR(50) PRIMARY KEY, 
+            password VARCHAR(30) NOT NULL
+            )";
 
-$sql = "CREATE TABLE users (
-username VARCHAR(50) PRIMARY KEY, 
-password VARCHAR(30) NOT NULL
-)";
-
-$result = mysqli_query($con, $sql);
-if ($result) {
-    echo "Table users created successfully";
-} else {
-    echo "Error creating table: " . mysqli_error($con);
+        $result = mysqli_query($con, $sql);
+        if ($result) {
+            echo "Table users created successfully";
+        } else {
+            echo "Error creating table: " . mysqli_error($con);
+        }
 }
+else echo "PRC";
+
+
 mysqli_close($con);
 ?>
