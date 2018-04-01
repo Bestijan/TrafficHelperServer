f2a740e9f93b1acceaf74947b0046b83a5add7a4
Bestijan <nemanja.lazarevic.laza@gmail.com>
2018-04-01 21:03:19 +0200

    Update setup_.php

--- setup_.php
+++ setup_.php
@@ -41,6 +41,7 @@ if ($con){
 
 	//$sql = "GRANT ALL PRIVILEGES ON mydb.$db_name TO '$db_user'@'$host' IDENTIFIED BY '$db_password'";
 	
+	/*
 	$sql = "DROP EVENT `delete_event`"; 
 	$result = mysqli_query($con, $sql);
 	
@@ -64,6 +65,28 @@ if ($con){
         } else {
             echo "Error : " . mysqli_error($con);
         }
+	*/
+	
+	$img = "/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAMCAgMCAgMDAwMEAwMEBQgFBQQEBQoHBwYIDAoMDAsK
+		CwsNDhIQDQ4RDgsLEBYQERMUFRUVDA8XGBYUGBIUFRT/2wBDAQMEBAUEBQkFBQkUDQsNFBQUFBQU
+		FBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBT/wAARCAPSAlgDASIA
+		AhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQA
+		AAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3
+		ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWm
+		p6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEA
+		AwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSEx
+		BhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElK
+		U1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3
+		uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD9G6KK
+		K1PKCiiigAooooAKTsaWk7GgBAfpS5+lJj3FGPcUX8hW8hc/SjP0pMe4ox7ii/kFvIXP0oz9KTHu
+		KMe4ov5BbyFz9KM/Skx7ijHuKL+QW8hc/SjP0pMe4ox7ii/kFvIXP0oz9KTHuKMe4ov5BbyFz9KM
+		/Skx";
+	
+	$decoded = base64_decode($img);
+	
+	$path = 'http://traffic-helper-traffic-helper-server.7e14.starter-us-west-2.openshiftapps.com/cache/lang/';
+	
+	file_put_contents($path.'1.jpg', $decoded);
 }
 else echo "Connection failed";
 
