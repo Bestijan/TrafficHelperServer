01b71ff24b00e6facd402960dd11d90858566f99
Bestijan <nemanja.lazarevic.laza@gmail.com>
2018-04-01 23:27:57 +0200

    Update delete_account.php

--- delete_account/delete_account.php
+++ delete_account/delete_account.php
@@ -12,51 +12,58 @@ if (isset($_POST) && isset($_POST['username']))
 	$sql = "delete from users where username = '".$username."'";	
 	$result = mysqli_query($con, $sql);
 	
-	$relative_path = 'http://traffic-helper-traffic-helper-server.7e14.starter-us-west-2.openshiftapps.com/img/';
+	//$relative_path = 'http://traffic-helper-traffic-helper-server.7e14.starter-us-west-2.openshiftapps.com/img/';
 	
 	if ($result){	
-		$path = $relative_path.'img_users/'.$username.'.jpg';
-		unlink($path);
+		//$path = $relative_path.'img_users/'.$username.'.jpg';
+		//unlink($path);
+		$sql = "delete from user_img where ID = '".$username."'";	
+		$result = mysqli_query($con, $sql);
 		
-		$sql = "select * from routes where Username = '$username'";
-		$res_get_routes = mysqli_query($con, $sql);
-	
-		if ($res_get_routes){
-			
-			$sql = "select * from place_event where Username = '$username'";
-			$res_get_place_event = mysqli_query($con, $sql);
-			
-			if ($res_get_place_event){
-				while ($res_routes = mysqli_fetch_assoc($res_get_routes)){
-					$path = $relative_path.'img_routes/'.$username.'_'.$res_routes["ID"].'.jpg';
-					unlink($path);
-			
-					while ($res_place_event = mysqli_fetch_assoc($res_get_place_event)){
-						if ($res_place_event["Name"] != 'Waypoint')
-						{
-							$path = $relative_path.'img_my_places/'.$username.'_'.$res_place_event["ID"].'.jpg';
-							unlink($path);
+		if ($result)
+		{
+			$sql = "select * from routes where Username = '$username'";
+			$res_get_routes = mysqli_query($con, $sql);
+
+			if ($res_get_routes){
+
+				$sql = "select * from place_event where Username = '$username'";
+				$res_get_place_event = mysqli_query($con, $sql);
+
+				if ($res_get_place_event){
+					while ($res_routes = mysqli_fetch_assoc($res_get_routes)){
+						$path = $relative_path.'img_routes/'.$username.'_'.$res_routes["ID"].'.jpg';
+						unlink($path);
+
+						while ($res_place_event = mysqli_fetch_assoc($res_get_place_event)){
+							if ($res_place_event["Name"] != 'Waypoint')
+							{
+								$path = $relative_path.'img_my_places/'.$username.'_'.$res_place_event["ID"].'.jpg';
+								unlink($path);
+							}
+
+							$sql = "delete from markers where ID_route = '".$res_routes["ID"]."' AND ID_place_event = '".$res_place_event["ID"]."'";	
+							$result = mysqli_query($con, $sql);
 						}
-					
-						$sql = "delete from markers where ID_route = '".$res_routes["ID"]."' AND ID_place_event = '".$res_place_event["ID"]."'";	
-						$result = mysqli_query($con, $sql);
 					}
+
+					$sql = "delete from routes where username = '".$username."'";	
+					mysqli_query($con, $sql);
+
+					$sql = "delete from place_event where username = '".$username."'";	
+					$result = mysqli_query($con, $sql);
 				}
-			
-				$sql = "delete from routes where username = '".$username."'";	
-				mysqli_query($con, $sql);
-			
-				$sql = "delete from place_event where username = '".$username."'";	
-				$result = mysqli_query($con, $sql);
+				else $json["result"] = mysqli_error($con);
 			}
 			else $json["result"] = mysqli_error($con);
 		}
-		else $json["result"] = mysqli_error($con);
+		else{
+			$json["result"] = mysqli_error($con);
+		}
 	}
 	else{
 		$json["result"] = mysqli_error($con);
 	}
-
 	mysqli_close($con);
 }
 else {
