cdb344ba98d21d16ab2387472f4ba410a999eb3c
Bestijan <nemanja.lazarevic.laza@gmail.com>
2018-04-01 02:49:06 +0200

    Add files via upload

---
+++ setup_.php
@@ -0,0 +1,15 @@
+<?php
+$host = getenv('MYSQL_SERVICE_HOST');
+$db_user = getenv('username');
+$db_password = getenv('password');
+$db_name = getenv('name');
+$con = mysqli_connect($host, $db_user, $db_password, $db_name);
+$sql = "create table users (username VARCHAR(50) PRIMARY KEY, password VARCHAR(30) NOT NULL)";
+$result = mysqli_query($con, $sql);
+if ($result) {
+    echo "Table users created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+mysqli_close($con);
+?>
