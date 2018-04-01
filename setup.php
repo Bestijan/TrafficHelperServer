6b4589b861fb872e8f38651486f9953e23f50ed4
Bestijan <nemanja.lazarevic.laza@gmail.com>
2018-04-02 01:01:25 +0200

    Update setup.php

--- setup.php
+++ setup.php
@@ -7,7 +7,9 @@ $db_password = getenv('password');
 $db_name = getenv('name');
 $con = mysqli_connect($host, $db_user, $db_password, $db_name);
 
-$sql = "insert into users (username, password) values ('Laza', 'lazalaza')";
+//$sql = "insert into users (username, password) values ('Laza', 'lazalaza')";
+
+$sql = "DELETE FROM place_event"; 
 
 $result = mysqli_query($con, $sql);
 
