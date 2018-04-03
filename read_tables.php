6c11cb778e1695de3b3277d5c4ea1e1b1a1491a2
Bestijan <nemanja.lazarevic.laza@gmail.com>
2018-04-03 20:27:51 +0200

    Add files via upload

--- read_tables.php
+++ read_tables.php
@@ -13,14 +13,20 @@
 		if ($result){	
 		
 			echo "place_event ";
-			echo "<br>";
+			echo '<html>
+					<br/>
+				  </html>';
 			
 			while ($row = mysqli_fetch_row($result)){
 				echo $row[0] . " " . $row[1] . " " . $row[2] . " " . $row[3] . " " . $row[4] . " " . $row[5] . " " . $row[6] . " " .$row[7];
-				each "<br>";
+			echo '<html>
+					<br/>
+				  </html>';
 			}
 			
-			echo "<br>";
+			echo '<html>
+					<br/>
+				  </html>';
 		}
 		else echo "Error reading table: " . $conn->error . "\r\n" . "\r\n" . "\r\n";
 		
