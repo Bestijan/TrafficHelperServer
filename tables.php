e56fafcbaa98b1ee6857a4a5998c518c9e94f270
Bestijan <nemanja.lazarevic.laza@gmail.com>
2018-04-03 23:24:22 +0200

    Add files via upload

---
+++ tables.php
@@ -0,0 +1,27 @@
+
+<?php
+	$host = getenv('MYSQL_SERVICE_HOST');
+	$db_user = getenv('username');
+	$db_password = getenv('password');
+	$db_name = getenv('name');
+	$con = mysqli_connect($host, $db_user, $db_password, $db_name);
+
+	if ($con) {
+		
+		$sql = "SELECT TABLE_NAME 
+			FROM INFORMATION_SCHEMA.TABLES
+			WHERE TABLE_TYPE = 'BASE TABLE' 
+			AND TABLE_SCHEMA='sampledb'";
+
+		$result = mysqli_query($con, $sql);
+		
+		if ($result){	
+			while ($result as $row){
+				echo $row . " ";
+			}
+		}
+ 	}
+	else echo "Error connecting: " . $conn->error;
+	
+	mysqli_close($con);
+?>
